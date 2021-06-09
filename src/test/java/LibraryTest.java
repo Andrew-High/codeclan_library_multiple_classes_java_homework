83691e2b08b580c9e423dde3aaa556a6a72f5984
Andrew High <andrewhigh4@hotmail.com>
2021-06-09 08:11:15 +0100

    passed 3 tests

---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LibraryTest {
+    private Library library;
+    private Book book;
+
+    @Before
+    public void before(){
+        library = new Library("Falkirk Library");
+        book = new Book("Northern Lights", "Philip Pullman", "Fantasy");
+    }
+
+    @Test
+    public void libraryHasName() {
+        assertEquals("Falkirk Library", library.getName());
+    }
+
+    @Test
+    public void libraryCollectionEmpty() {
+        assertEquals(0, library.getCollectionSize());
+    }
+
+    @Test
+    public void canAddBookToCollection() {
+        library.addBookToCollection(book);
+        assertEquals(1, library.getCollectionSize());
+    }
+}
